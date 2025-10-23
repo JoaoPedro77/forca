b8b56d9ae7fc17c3d58c037bca4a135b5287b2cf
João Pedro Cardoso <69486969+JoaoPedro77@users.noreply.github.com>
2025-10-23 19:35:30 -0300

    reestruturação do layout do app e melhora no componente header; add esquema de cores aleatorias; add modal de info; hifen agora funciona

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,13 @@
 <template>
   <UApp>
-    <UHeader to="/" :toggle="false">
-      <template #title>
-        <div class="flex items-center">
-          <Icon name="game-icons:gibbet" />
-          <h1>FORCA</h1>
-        </div>
-      </template>
-    </UHeader>
+
+    <!-- HEADER -->
+    <MeuHeader :cor-primaria="corPrimaria" />
 
     <UMain class="bg-stone-800 select-none">
+
+
+      <!-- TEMA E RESULTADO FIMDE JOGO -->
       <UContainer class="pt-10 gap-2 flex flex-col items-center justify-center">
         <USeparator class=" w-40">
           <template #default>
@@ -18,69 +16,70 @@
             <span v-if="ganhoubloqueio" class="text-xs font-light text-stone-500">Ganhou!!!</span>
           </template>
         </USeparator>
-        <UBadge v-if="!perdeubloqueio && !ganhoubloqueio" icon="icon-park-twotone:honey" size="xl"
-          class="px-5 rounded-full" color="primary" variant="subtle">
-          Mariii
+        <UBadge v-if="!perdeubloqueio && !ganhoubloqueio" :icon="iconeTema" size="xl" class=" rounded-full"
+          :color="corPrimaria" variant="subtle">
+          {{ tema }}
         </UBadge>
         <UBadge v-if="perdeubloqueio" icon="streamline-plump:skull-2-solid" size="lg" class="px-4 rounded-full"
           color="tertiary" variant="subtle">
           A {{ palavraOuFrase }} era: {{ palavra }}</UBadge>
 
         <UBadge v-if="ganhoubloqueio" icon="solar:confetti-bold-duotone" size="lg" class="px-4 rounded-full"
-          color="secondary" variant="subtle">
+          :color="corPrimaria" variant="subtle">
           Parabéns !!!</UBadge>
       </UContainer>
 
+
+      <!-- FORCA E PALAVRA -->
       <UContainer class="mb-10 flex flex-col gap-5 items-center justify-center">
-
         <img :src="`/forca/${forcaTentativa}.png`" class="h-50 sm:h-60 pl-7" />
-
         <div class="flex flex-wrap gap-1">
           <div v-for="(letraPalavra, i) in palavra" key="i">
-            <UBadge v-if="!chutes.some(l => letraPalavra.includes(l)) && letraPalavra != ' '" color="tertiary"
-              variant="subtle" class="text-xl font-bold text-white
-              w-8 h-8 flex items-center justify-center p-0">
+            <UBadge v-if="!chutes.some(l => letraPalavra.includes(l)) && letraPalavra != ' ' && letraPalavra != '-'"
+              color="tertiary" variant="subtle" class="text-xl font-bold text-white
+              w-8 h-8 md:w-12 md:h-12 flex items-center justify-center p-0">
             </UBadge>
-            <UBadge v-if="chutes.some(l => letraPalavra.includes(l)) && letraPalavra != ' '" color="primary"
-              variant="subtle" class="text-xl font-bold text-white
-              w-8 h-8 flex items-center justify-center p-0">
+            <UBadge v-if="chutes.some(l => letraPalavra.includes(l)) && letraPalavra != ' ' && letraPalavra != '-'"
+              :color="corPrimaria" variant="subtle" class="text-xl font-bold text-white
+              w-8 h-8 md:w-12 md:h-12 flex items-center justify-center p-0">
               {{ letraPalavra }}
             </UBadge>
-            <div v-if="letraPalavra == ' '" class="w-8 h-8">
+            <div v-if="letraPalavra == ' '" class="w-3 md:w-5  ">
+            </div>
+            <div v-if="letraPalavra == '-'" class="h-8 w-5 flex items-center justify-center">
+              <div class="bg-stone-400 h-1 w-3 rounded-xs" />
             </div>
           </div>
         </div>
-
-
-
         <UButton icon="material-symbols:replay-rounded" @click="tentarNovamente" v-if="ganhoubloqueio || perdeubloqueio"
-          color="primary" class="text-md font-bold text-white
+          :color="corPrimaria" class="text-md font-bold text-white
           px-2 flex items-center justify-center">
           Jogar Novamente
         </UButton>
       </UContainer>
-      <UContainer class="flex flex-wrap gap-2 items-center justify-center">
 
+
+      <!-- TECLADO -->
+      <UContainer class="max-w-2xl flex flex-wrap gap-2 items-center justify-center">
         <div v-for="letra in letras" key="id">
           <UButton @click="chutar(letra)" v-if="letra.status === 'padrao'" :disabled="perdeubloqueio || ganhoubloqueio"
-            color="primary" class="text-xl font-bold text-white
-        w-8 h-8 flex items-center justify-center p-0">
+            :color="corPrimaria" class="text-xl md:text-2xl md:rounded-xl font-bold text-white
+        w-8 h-8 md:w-15 md:h-12 flex items-center justify-center p-0">
             {{ letra.letra }}
           </UButton>
-          <UButton v-if="letra.status === 'naoTem'" color="tertiary" disabled="" class="text-xl font-bold text-white
-        w-8 h-8 flex items-center justify-center p-0">
+          <UButton v-if="letra.status === 'naoTem'" color="tertiary" disabled="" class="text-xl md:text-2xl font-bold text-white
+        w-8 h-8 md:w-15 md:h-12 flex items-center justify-center p-0">
             {{ letra.letra }}
           </UButton>
-          <UButton v-if="letra.status === 'tem'" color="secondary" disabled="" class="text-xl font-bold text-white
-        w-8 h-8 flex items-center justify-center p-0">
+          <UButton v-if="letra.status === 'tem'" color="success" disabled="" class="text-xl md:text-2xl font-bold text-white
+        w-8 h-8 md:w-15 md:h-12 flex items-center justify-center p-0">
             {{ letra.letra }}
           </UButton>
         </div>
-
       </UContainer>
 
-      <!-- MODAIS -->
 
+      <!-- MODAIS -->
       <UModal :open="ganhou" :close="{ onClick: () => ganhou = false }">
         <template #title>
           <div class="flex items-center justify-center gap-2">
@@ -90,13 +89,13 @@
         </template>
         <template #body>
           <div class="flex flex-col gap-2 items-center justify-center">
-            <UBadge icon="streamline-plump:balloon-remix" size="xl" class="px-4 rounded-full" color="primary"
+            <UBadge icon="streamline-plump:balloon-remix" size="xl" class="px-4 rounded-full" :color="corPrimaria"
               variant="subtle">
               A {{ palavraOuFrase }} era: {{ palavra }}
             </UBadge>
             <img src="/forca/7.png" class="h-50" />
             <UButton icon="material-symbols:replay-rounded" @click="tentarNovamente"
-              v-if="ganhoubloqueio || perdeubloqueio" color="primary" class="mt-7 text-md font-bold text-white
+              v-if="ganhoubloqueio || perdeubloqueio" :color="corPrimaria" class="mt-7 text-md font-bold text-white
             px-2 flex items-center justify-center">
               Jogar Novamente
             </UButton>
@@ -120,43 +119,33 @@
             </UBadge>
             <img src="/forca/6.png" class="h-50 pl-7" />
             <UButton icon="material-symbols:replay-rounded" @click="tentarNovamente"
-              v-if="ganhoubloqueio || perdeubloqueio" color="primary" class="mt-7 text-md font-bold text-white
+              v-if="ganhoubloqueio || perdeubloqueio" :color="corPrimaria" class="mt-7 text-md font-bold text-white
             px-2 flex items-center justify-center">
               Jogar Novamente
             </UButton>
           </div>
         </template>
       </UModal>
+
+
     </UMain>
-
-    <USeparator class="bg-stone-800" color="tertiary" icon="fluent-emoji-high-contrast:motor-scooter" />
-    <UFooter class="bg-stone-800">
-      <template #default>
-      </template>
-      <template #left>
-        <p class="text-muted text-sm">©JoaoJ {{ new Date().getFullYear() }}</p>
-      </template>
-      <template #right>
-        <UButton icon="i-simple-icons-instagram" color="neutral" variant="ghost"
-          to="https://www.instagram.com/joaoj_pedroo/" target="_blank" aria-label="Instagram" />
-        <UButton icon="i-simple-icons-github" color="neutral" variant="ghost" to="https://github.com/JoaoPedro77"
-          target="_blank" aria-label="GitHub" />
-      </template>
-    </UFooter>
-
-
   </UApp>
 </template>
 
 <script setup>
-
 import { ref } from 'vue';
 
+
+// VARIÁVEIS 
+
+const corPrimaria = ref("primary");
+
 const palavra = ref("");
-const palavraOuFrase = ref("");;
+const palavraOuFrase = ref("");
+const tema = ref("");
+const iconeTema = ref("");
 
 const letrasCorretas = ref([]);
-
 const acertos = ref(0);
 
 const forcaTentativa = ref(0);
@@ -166,6 +155,19 @@ const perdeu = ref(false);
 const perdeubloqueio = ref(false);
 const ganhou = ref(false);
 const ganhoubloqueio = ref(false);
+
+const cores = [
+  "primary",
+  "red",
+  "orange",
+  "emerald",
+  "miku",
+  "blue",
+  "indigo",
+  "violet",
+  "purple",
+  "pink",
+];
 
 const letras = ref([
   { id: 1, letra: "A", status: 'padrao', alt: ['Á', 'À', 'Ã', 'Â', 'Ä'] },
@@ -196,10 +198,13 @@ const letras = ref([
   { id: 26, letra: "Z", status: 'padrao', alt: [''] }
 ]);
 
+
+// FUNÇÕES
+
 //poe as letras corretas na array letrasCorretas
 function carregarLetrasCorretas() {
   for (let p in palavra.value) {
-    if (!(letrasCorretas.value.includes(palavra.value[p])) && palavra.value[p] != ' ') {
+    if (!(letrasCorretas.value.includes(palavra.value[p])) && palavra.value[p] != ' ' && palavra.value[p] != '-') {
       letrasCorretas.value.push(palavra.value[p])
     }
   }
@@ -208,6 +213,8 @@ function carregarLetrasCorretas() {
 
 //verifica se tem as letras que foram chutadas
 function chutar(letra) {
+
+  //verifica se tem a letra
   for (let p in letrasCorretas.value) {
     if (letra.letra === letrasCorretas.value[p]) {
       chutes.value.push(letra.letra)
@@ -220,6 +227,7 @@ function chutar(letra) {
       }
     }
 
+    // verifica se tem alguma variação da letra
     for (let lc in letrasCorretas.value) {
       if (letra.alt.includes(letrasCorretas.value[lc])) {
         chutes.value.push(letrasCorretas.value[lc])
@@ -235,6 +243,7 @@ function chutar(letra) {
     if (letra.status == "tem") { break }
   }
 
+  //se n tiver a letra
   if (letra.status != "tem") {
     letra.status = "naoTem"
     forcaTentativa.value += 1
@@ -248,7 +257,9 @@ function chutar(letra) {
 
 // gera a palavra da vez
 function gerarPalavra() {
-  palavra.value = "teste"
+  palavra.value = "T es-te-um"
+  tema.value = "geral"
+  iconeTema.value = "solar:earth-bold-duotone"
   palavra.value = palavra.value.toUpperCase();
   if (palavra.value.includes(' ')) {
     palavraOuFrase.value = "frase"
@@ -259,12 +270,10 @@ function gerarPalavra() {
 
 //reinicia valores pra tentar novamente.
 function tentarNovamente() {
-
   for (let letra in letras.value) {
     letras.value[letra].status = 'padrao';
     console.log(letras.value[letra].status)
   }
-
   gerarPalavra();
   acertos.value = 0;
   forcaTentativa.value = 0;
@@ -277,13 +286,20 @@ function tentarNovamente() {
   carregarLetrasCorretas();
   console.log(palavra);
   console.log(letrasCorretas);
-
 }
 
+//carrega cor aleatória
+function carregarCorAleatoria() {
+  corPrimaria.value = cores[Math.floor(Math.random() * cores.length)]
+}
+
+
+// LIFECYCLE HOOKS
+
 onMounted(() => {
-  gerarPalavra()
+  carregarCorAleatoria();
+  gerarPalavra();
   carregarLetrasCorretas();
 })
-
 
 </script>
