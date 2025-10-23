23fd1e33ae1701024fb97b10ed2257e48cb10bbe
João Pedro Cardoso <69486969+JoaoPedro77@users.noreply.github.com>
2025-10-23 00:48:42 -0300

    correção do bug

--- app/app.vue
+++ app/app.vue
@@ -9,7 +9,8 @@
       </template>
     </UHeader>
 
-    <UMain class="bg-stone-800">
+    <UMain class="bg-stone-800 select-none drag">
+
       <UContainer class="pt-10 gap-2 flex flex-col items-center justify-center">
         <USeparator class=" w-40">
           <template #default>
@@ -22,7 +23,7 @@
 
       <UContainer class="mb-10 flex flex-col gap-5 items-center justify-center">
 
-        <img :src="`/forca/${forcaTentativa}.png`" class="h-50 sm:h-60" />
+        <img :src="`/forca/${forcaTentativa}.png`" class="h-50 sm:h-60 pl-7" />
 
         <div class="flex flex-wrap gap-1">
           <div v-for="(letraPalavra, i) in palavra" key="i">
@@ -110,31 +111,31 @@ const ganhou = ref(false)
 
 const letras = ref([
   { id: 1, letra: "A", status: 'padrao', alt: ['Á', 'À', 'Ã', 'Â', 'Ä'] },
-  { id: 2, letra: "B", status: 'padrao' },
+  { id: 2, letra: "B", status: 'padrao', alt: [''] },
   { id: 3, letra: "C", status: 'padrao', alt: ['Ç'] },
-  { id: 4, letra: "D", status: 'padrao' },
+  { id: 4, letra: "D", status: 'padrao', alt: [''] },
   { id: 5, letra: "E", status: 'padrao', alt: ['É', 'È', 'Ê', 'Ë'] },
-  { id: 6, letra: "F", status: 'padrao' },
-  { id: 7, letra: "G", status: 'padrao' },
-  { id: 8, letra: "H", status: 'padrao' },
+  { id: 6, letra: "F", status: 'padrao', alt: [''] },
+  { id: 7, letra: "G", status: 'padrao', alt: [''] },
+  { id: 8, letra: "H", status: 'padrao', alt: [''] },
   { id: 9, letra: "I", status: 'padrao', alt: ['Í', 'Ì', 'Î', 'Ï'] },
-  { id: 10, letra: "J", status: 'padrao' },
-  { id: 11, letra: "K", status: 'padrao' },
-  { id: 12, letra: "L", status: 'padrao' },
-  { id: 13, letra: "M", status: 'padrao' },
-  { id: 14, letra: "N", status: 'padrao' },
+  { id: 10, letra: "J", status: 'padrao', alt: [''] },
+  { id: 11, letra: "K", status: 'padrao', alt: [''] },
+  { id: 12, letra: "L", status: 'padrao', alt: [''] },
+  { id: 13, letra: "M", status: 'padrao', alt: [''] },
+  { id: 14, letra: "N", status: 'padrao', alt: [''] },
   { id: 15, letra: "O", status: 'padrao', alt: ['Ó', 'Ò', 'Õ', 'Ô', 'Ö'] },
-  { id: 16, letra: "P", status: 'padrao' },
-  { id: 17, letra: "Q", status: 'padrao' },
-  { id: 18, letra: "R", status: 'padrao' },
-  { id: 19, letra: "S", status: 'padrao' },
-  { id: 20, letra: "T", status: 'padrao' },
+  { id: 16, letra: "P", status: 'padrao', alt: [''] },
+  { id: 17, letra: "Q", status: 'padrao', alt: [''] },
+  { id: 18, letra: "R", status: 'padrao', alt: [''] },
+  { id: 19, letra: "S", status: 'padrao', alt: [''] },
+  { id: 20, letra: "T", status: 'padrao', alt: [''] },
   { id: 21, letra: "U", status: 'padrao', alt: ['Ú', 'Ù', 'Û', 'Ü'] },
-  { id: 22, letra: "V", status: 'padrao' },
-  { id: 23, letra: "W", status: 'padrao' },
-  { id: 24, letra: "X", status: 'padrao' },
-  { id: 25, letra: "Y", status: 'padrao' },
-  { id: 26, letra: "Z", status: 'padrao' }
+  { id: 22, letra: "V", status: 'padrao', alt: [''] },
+  { id: 23, letra: "W", status: 'padrao', alt: [''] },
+  { id: 24, letra: "X", status: 'padrao', alt: [''] },
+  { id: 25, letra: "Y", status: 'padrao', alt: [''] },
+  { id: 26, letra: "Z", status: 'padrao', alt: [''] }
 ]);
 
 function chutar(letra) {
@@ -149,29 +150,30 @@ function chutar(letra) {
     }
 
     for (let lc in letrasCorretas.value) {
-      console.log(lc)
       if (letra.alt.includes(letrasCorretas.value[lc])) {
         chutes.value.push(letrasCorretas.value[lc])
         letra.status = "tem"
         acertos.value += 1;
+        console.log("acertos " + acertos.value)
+        console.log("tamanho " + letrasCorretas.value.length)
         if (acertos.value === letrasCorretas.value.length) {
           ganhou.value = true
         }
-
+        break;
       }
     }
+    if (letra.status == "tem") { break }
+  }
 
-    if (letra.status) {
-      return
+  if (letra.status != "tem") {
+    letra.status = "naoTem"
+    forcaTentativa.value += 1
+    if (forcaTentativa.value === 6) {
+      perdeu.value = true
     }
-
   }
 
-  letra.status = "naoTem"
-  forcaTentativa.value += 1
-  if (forcaTentativa.value === 6) {
-    perdeu.value = true
-  }
+
 
 
 
